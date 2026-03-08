0bd674fa452b12b7d86b76f128aedb698f885b1e
hetarikia <155637463+hetarikia@users.noreply.github.com>
2026-03-08 16:30:39 -0400

    temp receiver assets

--- src/GetVRCFTTrackerDataNode.cs
+++ src/GetVRCFTTrackerDataNode.cs
@@ -5,9 +5,19 @@ using Warudo.Core.Graphs;
 using Warudo.Core.Scenes;
 using Warudo.Plugins.Core.Assets;
 
-namespace Rikia {
-    [NodeType(Id = "69e9c383-04ed-47c8-b6be-aacfec8977dd", Title = "GET_VRCFT_TRACKER_DATA", Category = "CATEGORY_MOTION_CAPTURE")]
-    public class GetVRCFTTrackerDataNode : Warudo.Core.Graphs.Node {
-        
+namespace Rikia
+{
+    [NodeType(Id = "69e9c383-04ed-47c8-b6be-aacfec8977dd", Title = "Get VRCFT Receiver Data", Category = "CATEGORY_MOTION_CAPTURE")]
+    public class GetVRCFTTrackerDataNode : Node
+    {
+        [DataInput(13)]
+        [Label("RECEIVER")]
+        public VRCFTReceiverAsset Receiver;
+        public bool IsTracked()
+        {
+            VRCFTReceiverAsset receiver = this.Receiver;
+            return receiver != null && receiver.IsTracked;
+        }
+        public Dictionary<string, float> BlendShapes() => this.Receiver?.LatestBlendShapes;
     }
 }
