30e42053fed0849a4a9d2bda27ba431caefd505b
hetarikia <155637463+hetarikia@users.noreply.github.com>
2026-03-08 13:45:11 -0400

    Basic node in place

---
+++ src/GetVRCFTTrackerDataNode.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Warudo.Core.Attributes;
+using Warudo.Core.Graphs;
+using Warudo.Core.Scenes;
+using Warudo.Plugins.Core.Assets;
+
+namespace Rikia {
+    [NodeType(Id = "69e9c383-04ed-47c8-b6be-aacfec8977dd", Title = "GET_OPENSEEFACE_TRACKER_DATA", Category = "CATEGORY_MOTION_CAPTURE")]
+    public class GetVRCFTTrackerDataNode : Warudo.Core.Graphs.Node {
+        
+    }
+}
